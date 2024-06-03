697c94081526a5dacf6ff93084e4964937e3df43
Juan F. P. Mosquera <juan.pulgarin-mosquera@psi.ch>
2024-06-03 18:33:24 +0200

    incoherentes

--- Black_P_Excitons/Incoherent_Photo_Emission.py
+++ Black_P_Excitons/Incoherent_Photo_Emission.py
@@ -14,7 +14,76 @@ def func_q_eq_0(x, c, σ, x0):
 def func_bands_q(x, c, x0):
     return  c *(x-x0)**2
 
-def Reading_Data(Q,direccion,retorno=5):
+def interpolador(direccion):
+    if direccion == 'kz':
+        fx = func_q_nq_0
+        fy = func_q_eq_0
+        fz = func_q_eq_0
+        ft = func_q_nq_0
+
+    if direccion == 'kx':
+        fx = func_q_eq_0
+        fy = func_q_eq_0
+        fz = func_q_nq_0
+        ft = func_q_nq_0
+
+    if direccion == 'ky':
+        fx = func_q_eq_0
+        fy = func_q_nq_0
+        fz = func_q_eq_0
+        ft = func_q_nq_0
+
+    return fx,fy,fz,ft
+
+def maximos(x1,y_exciton):
+    max_position = np.where( y_exciton == np.max( y_exciton )   )[0][0]
+
+    x_max = x1[max_position]
+    y_max = y_exciton[max_position]
+
+    return x_max,y_max
+
+def Reading_Data(QQ,path,retorno=5):
+    nombre=[]
+    for qq in range(len(QQ)):
+        a = str(QQ[qq])
+        if a=='0.0':
+            a='0.00'
+        nombre.append(a)
+
+    fname="./"+path+"/"+'px'+nombre[0]+'_py'+nombre[1]+'_pz'+nombre[2]+"_exc.h5"
+    ϵ_plus,ϕ_real_plus,ϕ_imag_plus =Reading.Read_Eigen_states(fname)
+    ϕ_plus = ϕ_real_plus + 1.0j*ϕ_imag_plus
+
+    fname="./"+path+"/"+'px'+nombre[0]+'_py'+nombre[1]+'_pz'+nombre[2]+"_exc_dens.h5"
+    ψ_plus = Reading.Read_Density(fname)
+
+    if retorno ==1:
+        return ϵ_plus
+
+    if retorno ==2:
+        return ϕ_plus
+
+    if retorno ==3:
+        return ψ_plus
+
+    if retorno ==4:
+        return ϵ_plus,ϕ_plus
+
+    if retorno ==5:
+        return ϵ_plus,ψ_plus
+
+    if retorno ==6:
+        return ϕ_plus,ψ_plus
+
+    if retorno ==7:
+        return ϵ_plus,ϕ_plus,ψ_plus
+
+def partition_weight(EQQ,β):
+    return np.exp(-β*EQQ)
+
+
+"""def Reading_Data(Q,direccion,retorno=5):
     zero = '0.00'
     Dp =  np.array([Q])
     Dm = -Dp
@@ -44,17 +113,21 @@ def Reading_Data(Q,direccion,retorno=5):
 
     if direccion =='ky' and retorno !=8:
         fname="./out_Hqpgw_plus_new_merlin/"+'px'+zero+'_py'+str(Dp[0])+'_pz'+zero+"_exc.h5"
+        #fname="./out_Hqpgw_ky_test/"+'px'+zero+'_py'+str(Dp[0])+'_pz'+zero+"_exc.h5"
         ϵ_plus,ϕ_real_plus,ϕ_imag_plus =Reading.Read_Eigen_states(fname)
         ϕ_plus = ϕ_real_plus + 1.0j*ϕ_imag_plus
 
         fname="./out_Hqpgw_plus_new_merlin/"+'px'+zero+'_py'+str(Dp[0])+'_pz'+zero+"_exc_dens.h5"
+        #fname="./out_Hqpgw_ky_test/"+'px'+zero+'_py'+str(Dp[0])+'_pz'+zero+"_exc_dens.h5"
         ψ_plus = Reading.Read_Density(fname)
         #--------------------------------------------------------------------------------------------
         fname="./out_Hqpgw_minus_new_merlin/"+'px'+zero+'_py'+str(Dm[0])+'_pz'+zero+"_exc.h5"
+        #fname="./out_Hqpgw_ky_test/"+'px'+zero+'_py'+str(Dm[0])+'_pz'+zero+"_exc.h5"
         ϵ_minus,ϕ_real_minus,ϕ_imag_minus =Reading.Read_Eigen_states(fname)
         ϕ_minus = ϕ_real_minus + 1.0j*ϕ_imag_minus
 
         fname="./out_Hqpgw_minus_new_merlin/"+'px'+zero+'_py'+str(Dm[0])+'_pz'+zero+"_exc_dens.h5"
+        #fname="./out_Hqpgw_ky_test/"+'px'+zero+'_py'+str(Dm[0])+'_pz'+zero+"_exc_dens.h5"
         ψ_minus = Reading.Read_Density(fname)
 
     if direccion =='kz' and retorno !=8:
@@ -94,7 +167,7 @@ def Reading_Data(Q,direccion,retorno=5):
         return ϵ_plus,ϕ_plus,ψ_plus,ϕ_minus,ψ_minus
 
     if retorno ==8:
-        return ϵ_plus,ψ_plus
+        return ϵ_plus,ψ_plus"""
 
 def Plotting_Exitonic_Density(x1,nkx,nky,nkz,ψ,Qd,direccion):
     #====================================================================================
@@ -220,35 +293,56 @@ def Plotting_Exitonic_Density(x1,nkx,nky,nkz,ψ,Qd,direccion):
 
     plt.show()
 
-def Fitting_Parameters(x1,nkx,nky,nkz,ψ,direccion,QQ):
-    if direccion == False:
-        poptx, pcovx = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[:,int(nky/2),int(nkz/2)], bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
-        popty, pcovy = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[int(nkx/2),:,int(nkz/2)], bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
-        poptz, pcovz = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[int(nkx/2),int(nky/2),:], bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
-        poptx[2] = 0.0
-        popty[2] = 0.0
-        poptz[2] = 0.0
-
+"""def Fitting_Parameters(x1,nkx,nky,nkz,ψ,direccion,QQ):
     if direccion =='kx':
-        poptx, pcovx = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[:,int(nky/2),int(nkz/2)], bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
-        popty, pcovy = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[int(nkx/2),:,int(nkz/2)], bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
-        poptz, pcovz = curve_fit( func_q_nq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[int(nkx/2),int(nky/2),:],p0=np.array([1,1,1,-np.sign(QQ)]), bounds=((-np.inf,-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf,np.inf)) )
+        poptx, pcovx = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[:,int(nky/2),int(nkz/2)],p0=np.array([1,1,0]), bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
+        popty, pcovy = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[int(nkx/2),:,int(nkz/2)],p0=np.array([1,1,0]), bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
+        poptz, pcovz = curve_fit( func_q_nq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[int(nkx/2),int(nky/2),:],p0=np.array([1,1,-QQ,-np.sign(QQ)]), bounds=((-np.inf,-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf,np.inf)), method = 'trf' )
         poptx[2] = 0.0
         popty[2] = 0.0
 
     if direccion =='ky':
-        poptx, pcovx = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[:,int(nky/2),int(nkz/2)], bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
+        poptx, pcovx = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[:,int(nky/2),int(nkz/2)],p0=np.array([1,1,0]), bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
         #popty, pcovy = curve_fit( func_q_nq_0, x1[int(nky/2)-10:int(nky/2)+10],np.reshape(ψ[0,:], [nkx,nky,nkz])[int(nkx/2),:,int(nkz/2)][int(nky/2)-10:int(nky/2)+10] )
-        popty, pcovy = curve_fit( func_q_nq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[int(nkx/2),:,int(nkz/2)],p0=np.array([1,1,1,np.sign(QQ)]), bounds=((-np.inf,-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf,np.inf)) )
-        poptz, pcovz = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[int(nkx/2),int(nky/2),:], bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
+        popty, pcovy = curve_fit( func_q_nq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[int(nkx/2),:,int(nkz/2)],p0=np.array([1,1,-QQ,-np.sign(QQ)]), bounds=((-np.inf,-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf,np.inf)), method = 'trf', max_nfev=1000 )
+        poptz, pcovz = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[int(nkx/2),int(nky/2),:],p0=np.array([1,1,0]), bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
         poptx[2] = 0.0
         poptz[2] = 0.0
 
     if direccion =='kz':
-        poptx, pcovx = curve_fit( func_q_nq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[:,int(nky/2),int(nkz/2)],p0=np.array([1,1,1,-np.sign(QQ)]), bounds=((-np.inf,-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf,np.inf)) )
-        popty, pcovy = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[int(nkx/2),:,int(nkz/2)], bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
-        poptz, pcovz = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[int(nkx/2),int(nky/2),:], bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
+        poptx, pcovx = curve_fit( func_q_nq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[:,int(nky/2),int(nkz/2)],p0=np.array([1,1,-QQ,-np.sign(QQ)]), bounds=((-np.inf,-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf,np.inf)), method = 'trf' )
+        popty, pcovy = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[int(nkx/2),:,int(nkz/2)],p0=np.array([1,1,0]), bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)))
+        poptz, pcovz = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[int(nkx/2),int(nky/2),:],p0=np.array([1,1,0]), bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)))
+        popty[2] = 0.0
+        poptz[2] = 0.0
+
+    return poptx,popty,poptz"""
+
+def Fitting_Parameters(x1,nkx,nky,nkz,ψ,direccion,QQ,pos_fix):
+    #pos_fix = np.where(abs(x1-QQ/2)<=0.001)[0][0]
+    if direccion =='kx':
+        poptx, pcovx = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[:,pos_fix[1][0],pos_fix[2][0]],p0=np.array([1,1,0]), bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
+        popty, pcovy = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[pos_fix[0][0],:,pos_fix[2][0]],p0=np.array([1,1,0]), bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
+        poptz, pcovz = curve_fit( func_q_nq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[pos_fix[0][0],pos_fix[1][0],:],p0=np.array([1,1,-QQ,-np.sign(QQ)]), bounds=((-np.inf,-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf,np.inf)), method = 'trf' )
+        poptx[2] = 0.0
+        popty[2] = 0.0
+
+    if direccion =='ky':
+        poptx, pcovx = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[:,pos_fix[1][0],pos_fix[2][0]],p0=np.array([1,1,0]), bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
+        #popty, pcovy = curve_fit( func_q_nq_0, x1[int(nky/2)-10:int(nky/2)+10],np.reshape(ψ[0,:], [nkx,nky,nkz])[int(nkx/2),:,int(nkz/2)][int(nky/2)-10:int(nky/2)+10] )
+        popty, pcovy = curve_fit( func_q_nq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[pos_fix[0][0],:,pos_fix[2][0]],p0=np.array([1,1,-QQ,-np.sign(QQ)]), bounds=((-np.inf,-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf,np.inf)), method = 'trf', max_nfev=1000 )
+        poptz, pcovz = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[pos_fix[0][0],pos_fix[1][0],:],p0=np.array([1,1,0]), bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)) )
+        poptx[2] = 0.0
+        poptz[2] = 0.0
+
+    if direccion =='kz':
+        poptx, pcovx = curve_fit( func_q_nq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[:,pos_fix[1][0],pos_fix[2][0]],p0=np.array([1,1,-QQ,-np.sign(QQ)]), bounds=((-np.inf,-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf,np.inf)), method = 'trf' )
+        popty, pcovy = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[pos_fix[0][0],:,pos_fix[2][0]],p0=np.array([1,1,0]), bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)))
+        poptz, pcovz = curve_fit( func_q_eq_0, x1,np.reshape(ψ[0,:], [nkx,nky,nkz])[pos_fix[0][0],pos_fix[1][0],:],p0=np.array([1,1,0]), bounds=((-np.inf,-np.inf,-np.inf), (np.inf,np.inf,np.inf)))
         popty[2] = 0.0
         poptz[2] = 0.0
 
     return poptx,popty,poptz
+
+
+
